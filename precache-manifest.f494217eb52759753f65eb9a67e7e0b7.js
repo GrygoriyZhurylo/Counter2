e47a3f70e68b9e7765a9992ccd0332d6e3317e6e
Greg <grygoriy23@gmail.com>
2020-07-23 22:54:14 -0500

    Updates

---
+++ precache-manifest.f494217eb52759753f65eb9a67e7e0b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65d8c27508b5c2b74139bb097ecfb16",
+    "url": "/Counter2.git/index.html"
+  },
+  {
+    "revision": "31b9d1f5c0ba7f32b2f2",
+    "url": "/Counter2.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a1df246e2a3256ce676e",
+    "url": "/Counter2.git/static/css/main.b85adfc7.chunk.css"
+  },
+  {
+    "revision": "31b9d1f5c0ba7f32b2f2",
+    "url": "/Counter2.git/static/js/2.8d005beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter2.git/static/js/2.8d005beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1df246e2a3256ce676e",
+    "url": "/Counter2.git/static/js/main.4f95fb39.chunk.js"
+  },
+  {
+    "revision": "7c5a27ff671c75a3a1c7",
+    "url": "/Counter2.git/static/js/runtime-main.a39a5a1f.js"
+  }
+]);
